b644c812b266afd28527fa3b6223cd16118bb7e3
kotg <karwoo.tang@gft.com>
2018-04-04 11:07:07 +0100

    refactored

--- dashboard/urls.py
+++
@@ -1,7 +0,0 @@
-from .api import CashAgreementViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r'cashagreement', CashAgreementViewSet)
-
-urlpatterns = router.urls
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dademo.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dademo_service.settings")
 
     from django.core.management import execute_from_command_line
 
